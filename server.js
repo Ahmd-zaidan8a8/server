d32acf2360ff9daad62a1fc4af5947cd56ab0690
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-05-16 15:25:36 +0200

    add the env

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.ORIGIN || "http://localhost:5173" ,
     methods: "GET,POST ,DELETE , PUT",
   })
 );
