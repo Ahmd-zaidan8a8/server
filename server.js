902d9918bceba39255b3bdd3bb4683892adc06cc
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-05-16 15:05:17 +0200

    refactor

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const Joi = require("joi");
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
@@ -10,19 +9,16 @@ app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: "GET,POST",
+    methods: "GET,POST ,DELETE , PUT",
   })
 );
 
 const users = [];
 const consumedMeals = [];
 
-
-app.get("/", (req, res) => {
-  res.send("Hello! , Ahmad");
-});
-
+// TODO: add the user and validate it
 app.get("/login", (req, res) => {
+  // users.push()
   res.send(users);
 });
 
@@ -44,7 +40,10 @@ app.post("/login", (req, res) => {
   users.push(newUser);
   res.send(newUser);
 });
+
+// GET : Edmama API
 app.get("/nutrition-data", async (req, res) => {
+  console.log("get to edmama called");
   try {
     const response = await axios.get(
       "https://api.edamam.com/api/nutrition-data",
@@ -63,42 +62,59 @@ app.get("/nutrition-data", async (req, res) => {
   }
 });
 
+// GET /summarylist - get a list of meals
+app.get("/summarylist", (req, res) => {
+  res.status(200).json({
+    meals: consumedMeals,
+  });
+});
+
+// POST /summarylist - create new meal
 app.post("/summarylist", (req, res) => {
-  const {error , value} = validateMeal(req.body);
-
-
-  if(error) return res.status(400).send(error.details[0].message);
-
-  
-  const {dailyCalories , ingr , mealCount} = value;
+  const { dailyCalories, ingr } = req.body;
 
   const newMeal = {
-    id: uuidv4(),
+    id: consumedMeals.length + 1,
     dailyCalories,
     ingr,
-    mealCount,
   };
 
   consumedMeals.push(newMeal);
 
-  res
-    .status(201)
-    .json({
-      message: "You have successfully added a new meal in your daily plan",
-      mealInfo: newMeal,
-    });
-    console.log('adding a new Meal');
+  res.status(201).json({
+    message: "You have successfully added a new meal in your daily plan",
+    mealInfo: newMeal,
   });
-  function validateMeal(meal) {
-    const schema = Joi.object({
-      dailyCalories: Joi.number().min(0).required(),
-      ingr: Joi.array().items(Joi.string()).required(),
-      mealCount: Joi.number().min(1)
-    });
-    return schema.validate(meal);
-  }
-  
-  
+  console.log("adding a new Meal");
+});
+
+// Delete
+app.delete("/summarylist/:mealId", (req, res) => {
+  const meal = consumedMeals.find((meal) => {
+    return meal.id === parseInt(req.params.mealId);
+  });
+
+  if (!meal)
+    return res.status(404).send("the meal with the given id is not found");
+
+  const index = consumedMeals.indexOf(meal);
+  consumedMeals.splice(index, 1);
+  res.send(meal);
+});
+
+// Update
+app.put("/summarylist/:mealId", (req, res) => {
+  const meal = consumedMeals.find((meal) => {
+    return meal.id === parseInt(req.params.mealId);
+  });
+
+  if (!meal)
+    return res.status(404).send("the meal with the given id is not found!");
+
+  meal.ingr = req.body.ingr;
+  res.send(meal);
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listining on port ${port}`));
 
@@ -112,9 +128,6 @@ function validateUser(user) {
 
   return schema.validate(user);
 }
-
-
-
 
 axios.interceptors.request.use(
   function (config) {
